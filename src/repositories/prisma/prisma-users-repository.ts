f8a2d96dcd742f9ef413a6ae1de4d6e2434d7820
Bruno Rodrigues <brunowebr@gmail.com>
2025-11-23 12:53:27 -0300

    feat: create Prisma repository to persist data in DB

---
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -0,0 +1,28 @@
+import { Users } from 'prisma/generated/client';
+import { UsersCreateInput } from 'prisma/generated/models';
+import { UsersRepository } from '../user-repository';
+import { prisma } from '@/lib/prisma';
+
+export class PrismaUsersRepository implements UsersRepository {
+  async create(data: UsersCreateInput) {
+    const user = await prisma.users.create({
+      data
+    });
+
+    return user;
+  }
+
+  async findByEmail(email: string) {
+    const userByEmail = await prisma.users.findUnique({
+      where: {
+        email
+      }
+    });
+
+    if (userByEmail) {
+      return userByEmail;
+    }
+
+    return null;
+  }
+}
