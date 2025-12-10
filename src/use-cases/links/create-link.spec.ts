c7b979776e5472f9193138d9a5b407083a23e23e
Bruno Rodrigues <brunowebr@gmail.com>
2025-12-10 08:35:35 -0300

    feat: update Create link use case to add Pages as dependency to avoid create a link with a invalid/inexistent page

--- src/use-cases/links/create-link.spec.ts
+++ src/use-cases/links/create-link.spec.ts
@@ -2,6 +2,8 @@ import { describe, expect, it } from 'vitest';
 import { faker } from '@faker-js/faker/locale/pt_BR';
 import { CreateLinkUseCase } from './create-link';
 import { InMemoryLinksRepository } from '@/repositories/in-memory/im-links-repository';
+import { InMemoryPagesRepository } from '@/repositories/in-memory/im-pages-respository';
+import { ResourceNotFoundError } from '../errors/not-found-error';
 
 
 
@@ -9,7 +11,16 @@ describe('Create links use case', () => {
 
   it('should be able to create a new link', async () => {
     const inMemoryLinksRepository = new InMemoryLinksRepository();
-    const createLinkUseCase = new CreateLinkUseCase(inMemoryLinksRepository);
+    const inMemoryPagesRepository = new InMemoryPagesRepository();
+    const createLinkUseCase = new CreateLinkUseCase(inMemoryLinksRepository, inMemoryPagesRepository);
+
+    inMemoryPagesRepository.create({
+      userId: 'user-001',
+      id: 'page-001',
+      title: 'TS Page',
+      description: 'A simple page',
+      slug: 'my-new-page',
+    });
 
     const { link } = await createLinkUseCase.execute({
       title: faker.lorem.words(5),
@@ -20,5 +31,19 @@ describe('Create links use case', () => {
     });
 
     expect(link.id).toEqual(expect.any(String));
+  });
+
+  it('should not be able to create a new link from a non existent page', async () => {
+    const inMemoryLinksRepository = new InMemoryLinksRepository();
+    const inMemoryPagesRepository = new InMemoryPagesRepository();
+    const createLinkUseCase = new CreateLinkUseCase(inMemoryLinksRepository, inMemoryPagesRepository);
+
+    await expect(createLinkUseCase.execute({
+      title: faker.lorem.words(5),
+      description: faker.lorem.words(20),
+      url: faker.internet.url(),
+      pageId: 'page-001',
+      type: 'url'
+    })).rejects.toBeInstanceOf(ResourceNotFoundError);
   });
 });
--- src/use-cases/links/create-link.ts
+++ src/use-cases/links/create-link.ts
@@ -1,4 +1,6 @@
 import { LinksRepository } from '@/repositories/links-repository';
+import { PagesRepository } from '@/repositories/pages-repository';
+import { ResourceNotFoundError } from '../errors/not-found-error';
 
 interface CreateLinkUseCaseRequest {
   title: string
@@ -9,9 +11,18 @@ interface CreateLinkUseCaseRequest {
 }
 
 export class CreateLinkUseCase {
-  constructor(private linksRepository: LinksRepository) { }
+  constructor(
+    private linksRepository: LinksRepository,
+    private pagesRepository: PagesRepository
+  ) { }
 
   async execute(data: CreateLinkUseCaseRequest) {
+    const doesPageExists = await this.pagesRepository.findById(data.pageId);
+
+    if (!doesPageExists) {
+      throw new ResourceNotFoundError('Page not found');
+    }
+
     const link = await this.linksRepository.create(data);
 
     return { link };
