eb9c9e93b9747101ec5ab121eb9f55f9a67c16f9
Bruno Rodrigues <brunowebr@gmail.com>
2026-01-02 12:20:29 -0300

    feat: update auth test to add the create controller

--- src/http/controllers/users/authenticate.spec.ts
+++ src/http/controllers/users/authenticate.spec.ts
@@ -2,7 +2,6 @@ import { beforeEach, describe, expect, it } from 'vitest';
 import request from 'supertest';
 import { app } from '@/app';
 import { prisma } from '@/lib/prisma';
-import { encryptString } from '@/utils/encrypt-string';
 
 describe('Authenticate E2E test', () => {
   beforeEach(async () => {
@@ -11,14 +10,11 @@ describe('Authenticate E2E test', () => {
 
   it('should be able to authenticate', async () => {
 
-    // TO-DO: Update to user creation controller
-    await prisma.users.create({
-      data: {
-        fullname: 'Manager',
-        email: 'manager+test@linksforall.com',
-        password: await encryptString('123456')
-      }
-    });
+    await request(app).post('/users').send({
+      fullname: 'Manager',
+      email: 'manager+test@linksforall.com',
+      password: '123456'
+    }).set('Accept', 'application/json');
 
     const response = await request(app).post('/auth').send({
       email: 'manager+test@linksforall.com',
